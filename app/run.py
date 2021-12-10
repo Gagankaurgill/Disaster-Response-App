7fcf4ecc2987d55e00caae38a34c26871644d3a4
Gagankaurgill <33652333+Gagankaurgill@users.noreply.github.com>
2021-12-10 20:22:27 +0530

    Updated run.py

--- app/run.py
+++ app/run.py
@@ -2,13 +2,13 @@ import json
 import plotly
 import pandas as pd
 import re
+import joblib
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
 
